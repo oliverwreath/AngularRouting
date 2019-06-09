c9ff3604d51e267f8e72867aa03feeb3ba6c5f9b
oliverwreath <skywalkerhunter@gmail.com>
2019-06-09 01:38:33 -0400

    login page. and the important principle of using layout component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,32 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
+import {LoginComponent} from './login/login.component';
+import {LayoutComponent} from './layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '',
-    component: HomeComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '**',
