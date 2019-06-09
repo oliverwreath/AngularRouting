aafaf32dead052b890fd807b09fd6cc576d546f5
oliverwreath <skywalkerhunter@gmail.com>
2019-06-08 22:53:17 -0400

    added routing and routing test. added 2 pages, the home component and the about component.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true,
+    useHash: true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
