38f930708d840d8c7dc400eb90dcad8ae28ea470
oliverwreath <skywalkerhunter@gmail.com>
2019-06-11 00:30:48 -0400

    import RouterTestingModule is necessary for Routing components. with preloading strategy.

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -8,9 +9,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LoginComponent
+      ],
+      imports: [
+        RouterTestingModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
